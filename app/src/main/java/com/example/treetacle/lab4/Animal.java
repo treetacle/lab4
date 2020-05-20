90baa06eb32811f7942da049aeb5378ce55f1418
treetacle <treetacle@wir.pl>
2020-05-20 19:09:15 +0200

    1 lab5

---
+++ app/src/main/java/com/example/treetacle/lab4/Animal.java
@@ -0,0 +1,40 @@
+package com.example.treetacle.lab4;
+
+import java.io.Serializable;
+
+/**
+ * Created by Treetacle on 20.05.2020.
+ */
+
+public class Animal implements Serializable {
+
+    private int id;
+    private String gatunek;
+    private String kolor;
+    private float wielkosc;
+    private String opis;
+
+    public Animal(){}
+
+    public Animal(String gatunek, String kolor, float wielkosc, String opis){
+        super();
+        this.gatunek = gatunek;
+        this.kolor = kolor;
+        this.wielkosc = wielkosc;
+        this.opis = opis
+    }
+
+    @Override
+    public String toString(){
+        return "Zwierze: [id=" + id + ", gatunek=" + gatunek
+                + ", kolor=" + kolor + " wielkosc=" + wielkosc + "]";
+    }
+
+    public String getOpis(){ return opis; }
+    public String getGatunek(){ return gatunek; }
+    public String getKolor(){ return kolor; }
+    public float getWielkosc(){ return wielkosc; }
+    public int getId(){ return id; }
+    public void setId(int id){ this.id=id; }
+
+}
