581d12f336dc462ac650e2a43661353170487829
treetacle <treetacle@wir.pl>
2020-05-20 13:35:12 +0200

    5

--- app/src/main/java/com/example/treetacle/lab4/MainActivity.java
+++ app/src/main/java/com/example/treetacle/lab4/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.treetacle.lab4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,5 +31,20 @@ private ArrayAdapter adapter;
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter((this.adapter));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 }
